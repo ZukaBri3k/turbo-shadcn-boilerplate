f6d9ed19cc291dfb64f3b831ad396bf58fbd8641
kdumerchat <kyrill.dumerchat@kwh50.io>
2025-07-03 08:46:44 +0200

    update starter page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,9 +2,8 @@ import { Button } from "@repo/ui/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl text-red-500">Test</h1>
-      <Button>Test</Button>
+    <div className="flex h-screen items-center justify-center">
+      <Button>Shadcn Ui</Button>
     </div>
   );
 }
